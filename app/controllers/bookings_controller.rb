fb4a1ec8a1647849d9cbe05d092bd1f277b284ab
costindumitrescu <dumitrescucostin20@gmail.com>
2019-11-19 15:10:24 -0500

    worked on the bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,25 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+  def show
+    @booking = Booking.find(params[:id])
+  end
+  def new
+    @booking = Booking.new
+  end
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :status)
+  end
 end
