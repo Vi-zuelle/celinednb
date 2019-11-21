74d2c91c5ee319975a280bea321f38ae53da84a6
Vi <vipannetier@gmail.com>
2019-11-21 11:57:20 -0500

    bookings list per version with accept decline buttons. Need to make some Front end arrangements

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
   has_many :bookings
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  def full_name
+    name = first_name + " " + last_name 
+    return name
+  end
 end
